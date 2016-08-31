3619592e2efe2feb0630c28cbbea4188fa3136f2
vanesky <2538064643@qq.com>
2016-08-31 15:36:48 +0800

    jq查询优化

--- src/js/component.js
+++ src/js/component.js
@@ -5,7 +5,11 @@ window.com = {};
 
     this.load = function(){
 
-        var str = '<div id="load"> <div class="load-main"> <div class="animate"> <div class="animate-list"></div> <div class="animate-list"></div> <div class="animate-list"></div> <div class="animate-list"></div> </div> <span class="animate-text">正在加载 . .</span> </div> </div>';
+        var str = '<div id="load"><div class="load-main"><div class="animate">' +
+            '<div class="animate-list"></div>' +
+            '<div class="animate-list"></div>' +
+            '<div class="animate-list"></div>' +
+            '</div><span class="animate-text">正在加载 . .</span></div></div>';
 
         $('.container').append(str);
 
