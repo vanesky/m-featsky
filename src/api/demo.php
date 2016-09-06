9d24423c7d25858d24c20896436d8aaa3dad3891
vanesky <2538064643@qq.com>
2016-09-06 17:30:49 +0800

    页面修改 0.1.3

--- src/api/demo.php
+++ src/api/demo.php
@@ -6,22 +6,34 @@
     if (!$con){
 
         die('Could not connect: ' . mysql_error());
-
     }
+    mysql_select_db("featsky", $con);
+    /*============================================================*/
 
 
-    /*mysql_select_db("featsky", $con);
+    //首页图片
+    $imgResult = mysql_query("SELECT * FROM featsky where id=1");
+    $imgObj = mysql_fetch_array($imgResult);
+
+    //获取各个数量
+    //文章
+    $artResult = mysql_query("SELECT * FROM article");
+    $artList = mysql_num_rows($artResult);
+    //图片
+
+    //影视
+    $movieResult = mysql_query("SELECT * FROM movie");
+    $movieList = mysql_num_rows($movieResult);
+    //说说
+    $sayResult = mysql_query("SELECT * FROM say");
+    $sayList = mysql_num_rows($sayResult);
+
+    echo 123;
 
 
-    $createTable = "create table vip
-    (
-    uid int,
-    name varchar(15),
-    loginName varchar(20),
-    loginPass varchar(16),
-    headImg varchar(100),
-    mark varchar(50)
-    )";
-    mysql_query($createTable,$con);*/
+
+    mysql_close($con);
+
+
 
 ?>
