5c74008640544f65b0902826a63f4ef9fd01f0f2
vanesky <2538064643@qq.com>
2016-11-09 16:56:01 +0800

    1.0.6 hybrid

--- component/prompt/com_prompt.js
+++ component/prompt/com_prompt.js
@@ -1,10 +1,12 @@
 if(!window.com){window.com = {}};
 
-com.prompt = function(type,text,fun1,fun2){
+com.prompt = function(type,text,fun){
 
     var str = '<div id="com-prompt"><div class="prompt-main"><h5 class="prompt-tit">提示</h5><div class="prompt-text">0</div><ul class="prompt-item"> <li class="prompt-button bc-db c6">取消</li> <li class="prompt-button c-ba cf">确定</li> </ul> </div> </div>';
 
     var promptObj = $('#com-prompt');
+
+        promptObj.find('.prompt-button').off();
 
     if(!promptObj.css('display')){
 
@@ -29,22 +31,22 @@ com.prompt = function(type,text,fun1,fun2){
 
         promptObj.find('.prompt-button').eq(0).hide();
 
-        if(fun1){
+        if(fun){
 
             promptObj.find('.prompt-button').on('tap',function(){
 
-                fun1();
+                fun();
 
             })
         }
 
-    }else if(type == 1 && fun1 && fun2){
+    }else if(type == 1 && fun){
 
         promptObj.find('.prompt-button').on('tap',function(){
 
             var index = $(this).index();
 
-            0 == index ? fun1() : fun2();
+            if(index == 1){fun()}
 
         })
 
